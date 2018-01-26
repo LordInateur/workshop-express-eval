77485cfab404caa9294514b2d899a878a595a476
Matthieu Fournier <mat.fournier85@gmail.com>
2018-01-26 15:30:19 +0100

    Exercice 4 : readFile

--- src/app.js
+++ src/app.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 app.get('/bingo', function(req, res) {
   res.status(200);
-  res.end('Bingo')
+  getBingo((err, data) => {
+    if(err){
+      res.status(400);
+      res.end(JSON.stringify(err))
+    }else{
+      res.status(200);
+      res.end(data)
+    }
+  })
 });
 
+function getBingo(callback){
+  getfile('src/numbers.txt', (err, data) => {
+    if(err){
+      callback(err, undefined);
+    }else{
+      callback(undefined, data.split('\r\n').slice(0,-1).join());
+    }
+  })
+}
+
+function getfile(filePath, callback){
+  fs.readFile(filePath, (err, data) =>{
+    if(err){
+      callback(err, undefined);
+    }else{
+      callback(undefined, data.toString());
+    }
+  });
+}
 module.exports = app;
